bb2c6033dddce1ec07fbeec2efbe84747debc265
HasnainRaza026 <hasnainraza0026@gmail.com>
2024-12-08 07:51:34 +0500

    some changes

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     from eventlet import wsgi
     wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
 
+    # app.run(debug=True, host='127.0.0.1', port=5000)    # For Development and Testing
+
 
 
 '''
--- website/models.py
+++ website/models.py
@@ -4,12 +4,12 @@ class Top_10_Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(180), nullable=False)  # Removed unique constraint on description
+    description = db.Column(db.String(180), nullable=False)
     rating = db.Column(db.Float, nullable=False)
     rank = db.Column(db.Integer, unique=True, nullable=False)
     review = db.Column(db.String(20), nullable=False)
-    img_url = db.Column(db.String(1000), unique=True, nullable=False)  # No need to name this constraint
-    movie_id = db.Column(db.Integer, unique=True, nullable=False)  # No need to name this constraint
+    img_url = db.Column(db.String(1000), unique=True, nullable=False)
+    movie_id = db.Column(db.Integer, unique=True, nullable=False)
 
 
 class All_Movies(db.Model):
@@ -18,5 +18,5 @@ class All_Movies(db.Model):
     year = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     review = db.Column(db.String(20), nullable=False)
-    img_url = db.Column(db.String(1000), unique=True, nullable=False)  # No need to name this constraint
-    movie_id = db.Column(db.Integer, unique=True, nullable=False)  # No need to name this constraint
+    img_url = db.Column(db.String(1000), unique=True, nullable=False)
+    movie_id = db.Column(db.Integer, unique=True, nullable=False)
